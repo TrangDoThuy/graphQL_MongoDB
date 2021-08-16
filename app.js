0a0a80dd6ab180e1ea4c15465bc094f3a34b06a7
Emma Thuy Trang DO <30380242+TrangDoThuy@users.noreply.github.com>
2021-08-16 18:00:48 +0800

    First commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const mongoose = require('mongoose');
+const isAuth = require('./middleware/is-auth');
+
+const graphQlSchema = require('./graphql/schema/index');
+const graphQlResolvers = require('./graphql/resolvers/index');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+app.use(isAuth);
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: graphQlSchema,
+    rootValue: graphQlResolvers,
+    graphiql: true,
+  })
+);
+
+mongoose
+  .connect(
+    'mongodb+srv://user1:testing0501@cluster0.i4jre.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+  )
+  .then(() => {
+    app.listen(8000);
+    console.log('connected to database');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
